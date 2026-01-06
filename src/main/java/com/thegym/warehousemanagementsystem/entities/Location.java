6c7676c1eb4293d46193424b544c01744b3aaf7c
manzitresor <manzitre@gmail.com>
2026-01-06 15:04:00 +0200

    fix: remove unused imports

--- src/main/java/com/thegym/warehousemanagementsystem/entities/Location.java
+++ src/main/java/com/thegym/warehousemanagementsystem/entities/Location.java
@@ -1,8 +1,6 @@
 package com.thegym.warehousemanagementsystem.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
