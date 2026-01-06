fde937451ac8ba73ba30ac30c7c24b3c84cc6e8f
manzitresor <manzitre@gmail.com>
2026-01-06 15:02:18 +0200

    refactor: rename ConflictException to make global

---
+++ src/main/java/com/thegym/warehousemanagementsystem/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.thegym.warehousemanagementsystem.exceptions;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/thegym/warehousemanagementsystem/exceptions/DuplicateWarehouseException.java
+++
@@ -1,7 +0,0 @@
-package com.thegym.warehousemanagementsystem.exceptions;
-
-public class DuplicateWarehouseException extends RuntimeException {
-    public DuplicateWarehouseException(String message) {
-        super(message);
-    }
-}
