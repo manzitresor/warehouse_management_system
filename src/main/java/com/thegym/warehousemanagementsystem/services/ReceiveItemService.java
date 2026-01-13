e927c82fba2960c5e66ebdf6d673749fba71304e
manzitresor <manzitre@gmail.com>
2026-01-13 19:26:48 +0200

    refactor: save history when moving and receiving item

--- src/main/java/com/thegym/warehousemanagementsystem/services/ReceiveItemService.java
+++ src/main/java/com/thegym/warehousemanagementsystem/services/ReceiveItemService.java
@@ -2,16 +2,13 @@ package com.thegym.warehousemanagementsystem.services;
 
 
 import com.thegym.warehousemanagementsystem.dtos.ReceiveItemRequestDto;
-import com.thegym.warehousemanagementsystem.entities.Item;
-import com.thegym.warehousemanagementsystem.entities.Sscc;
+import com.thegym.warehousemanagementsystem.entities.*;
 import com.thegym.warehousemanagementsystem.exceptions.ConflictException;
 import com.thegym.warehousemanagementsystem.exceptions.ResourceNotFoundException;
-import com.thegym.warehousemanagementsystem.repositories.ItemRepository;
-import com.thegym.warehousemanagementsystem.repositories.LocationRepository;
-import com.thegym.warehousemanagementsystem.repositories.SsccRepository;
-import com.thegym.warehousemanagementsystem.repositories.WarehouseRepository;
+import com.thegym.warehousemanagementsystem.repositories.*;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -20,6 +17,7 @@ import java.time.Instant;
 @Service
 @AllArgsConstructor
 public class ReceiveItemService {
+    private final StockHistoryRepository stockHistoryRepository;
     private LocationRepository locationRepository;
     private WarehouseRepository warehouseRepository;
     private SsccRepository ssccRepository;
@@ -28,47 +26,55 @@ public class ReceiveItemService {
 
     @Transactional
     public Item create(ReceiveItemRequestDto requestDto) {
-        var warehouse = warehouseRepository.findByWarehouseNumber(requestDto.getWarehouseNumber()).orElse(null);
-        if (warehouse == null) {
-            throw new ResourceNotFoundException("Warehouse not found or is inactive.");
-        }
-
+        var warehouse = warehouseRepository.findByWarehouseNumber(requestDto.getWarehouseNumber()).orElseThrow(()->new ResourceNotFoundException("Warehouse not found"));
         if(Boolean.FALSE.equals(warehouse.getActive())){
             throw new ConflictException("Warehouse "+warehouse.getWarehouseNumber()+" is inactive.");
         }
 
-        var sscc = ssccRepository.findBySscc(requestDto.getSscc()).orElse(null);
-        if (sscc == null ) {
-            throw new ResourceNotFoundException("Sscc not found");
-        }
-
+        var sscc = ssccRepository.findBySscc(requestDto.getSscc()).orElseThrow(()->new ResourceNotFoundException("Sscc not found"));
         if(sscc.getReceivedTimestamp() != null ) {
-            throw new ConflictException("This Item is has been received.");
+            throw new ConflictException("This Item has been received.");
         }
 
 
         var cartonHeader = sscc.getCartonHeader();
-        var location = locationRepository.findByLocationCode(requestDto.getLocationCode()).orElse(null);
-        if(location == null) {
-            throw new ResourceNotFoundException("Location not found.");
-        }
+        var location = locationRepository.findByLocationCode(requestDto.getLocationCode()).orElseThrow(()->new ResourceNotFoundException("Location not found"));
 
         var existingItemOpt = itemRepository.findByItemNumberAndLocation(cartonHeader.getBarcode(), location);
         if(existingItemOpt.isPresent()) {
-            Item  existingItem = existingItemOpt.get();
+            Item existingItem = existingItemOpt.get();
             existingItem.setQuantity(existingItem.getQuantity() + 1);
             setReceivedTimeStamp(sscc);
+
+            saveHistory(cartonHeader, warehouse, sscc, existingItem);
             return itemRepository.save(existingItem);
         }
 
+        Item item = setItem(cartonHeader, location, sscc);
+        saveHistory(cartonHeader, warehouse, sscc, item);
+
+        return itemRepository.save(item);
+    }
+
+    private Item setItem(CartonHeader cartonHeader, Location location, Sscc sscc) {
         Item item = new Item();
         item.setItemNumber(cartonHeader.getBarcode());
         item.setQuantity(1);
         item.setCartonHeader(cartonHeader);
         item.setLocation(location);
         setReceivedTimeStamp(sscc);
+        return item;
+    }
 
-        return itemRepository.save(item);
+    private void saveHistory(CartonHeader cartonHeader, Warehouse warehouse, Sscc sscc, Item existingItem) {
+        StockHistory stockHistory = new StockHistory();
+        stockHistory.setAction(Action.RECEIVE);
+        stockHistory.setItemNumber(cartonHeader.getBarcode());
+        stockHistory.setWarehouseNumber(warehouse.getWarehouseNumber());
+        stockHistory.setSscc(sscc.getSscc());
+        stockHistory.setItemNumber(existingItem.getItemNumber());
+        stockHistory.setQuantityChange(existingItem.getQuantity());
+        stockHistoryRepository.save(stockHistory);
     }
 
     private void setReceivedTimeStamp(Sscc sscc) {
