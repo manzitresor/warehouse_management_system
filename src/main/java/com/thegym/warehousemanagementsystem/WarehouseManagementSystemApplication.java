789a4efc6a435819ed07ed96e3554adff7388dde
manzitresor <manzitre@gmail.com>
2026-01-29 17:11:16 +0200

    chore: add stockMovement consumer

--- src/main/java/com/thegym/warehousemanagementsystem/WarehouseManagementSystemApplication.java
+++ src/main/java/com/thegym/warehousemanagementsystem/WarehouseManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.thegym.warehousemanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class WarehouseManagementSystemApplication {
 
     public static void main(String[] args) {
